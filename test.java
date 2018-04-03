a6e15bc93b3b198382b05ed1b7ec7570e523c938
wqdeng <dengweiq@gmail.com>
2018-04-03 21:29:50 +0800

    ttttttttttt

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("test git");
-        System.out.println("hello git");
+        System.out.println("git");
     }
 }
