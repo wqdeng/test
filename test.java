ad3174c2b1e4291a5781ddb85667e1296425f9fa
wqdeng <dengweiq@gmail.com>
2018-04-13 19:51:56 +0800

    te

--- test.java
+++ test.java
@@ -10,7 +10,7 @@ public class Test {
             sadasd
 asdasdasdasdasd
 System.out.println("test");
-System.out.println("hello test");
+System.out.println("hello test world");
 System.out.println("test");
     }
 }
